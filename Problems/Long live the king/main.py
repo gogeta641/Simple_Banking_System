6b0c5cd5de02870b8c0bf364656e178f8ca264af
Karthik Ramachandran <gogeta641@users.noreply.github.com>
2020-12-07 23:18:29 +0530

    Add

--- Problems/Long live the king/main.py
+++ Problems/Long live the king/main.py
@@ -1,15 +1,9 @@
-x = int(input())
-y = int(input())
+col = int(input())
+row = int(input())
 
-if x == 1:
-    if y == 1 or y == 8:
-        print(3)
-    elif y < 8:
-        print(5)
-elif x == 8:
-    if y == 1 or y == 8:
-        print(3)
-    elif y < 8:
-        print(5)
-elif 1 < x < 8 and 1 < y < 8:
-    print(8)
+if col in (1, 8) and row in (1, 8):
+    print("3")
+elif col in (1, 8) or row in (1, 8):
+    print("5")
+else:
+    print("8")
